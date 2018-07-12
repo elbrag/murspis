1602085b09810e30f1e0c8dcecd1e5ed77450a34
elbrag <32334193+elbrag@users.noreply.github.com>
2018-07-12 16:38:13 +0200

    progress lightbox

--- galleri.php
+++ galleri.php
@@ -41,48 +41,77 @@ $query = new WP_Query( $args );
 
 <section id='galleri_2'>
 
-<?php
-if( $query->have_posts() ) {
-    while ( $query->have_posts() ) {
-      $query->the_post();
+    <div class='thumbnails'>
+        <?php
+        if( $query->have_posts() ) {
+            while ( $query->have_posts() ) {
+              $query->the_post();
 
-      $galleribild = get_field('galleribild');
-      $resized = $galleribild['sizes'][ 'grid_thumbnail' ];
-      $bildid = $galleribild['id'];
+              $galleribild = get_field('galleribild');
+              $resized = $galleribild['sizes'][ 'grid_thumbnail' ];
+              $bildid = $galleribild['id'];
 
-?>
+              ?>
 
-      <div class='galleripost' id='<?php echo $bildid ?>'>
-          <div class='galleribild' style='background-image:url(<?php echo $resized ?>)'></div>
-      </div>
+              <div class='galleripost' id='<?php echo $bildid ?>'>
+                  <div class='galleribild' style='background-image:url(<?php echo $resized ?>)'></div>
+              </div>
 
-    <?php }
-   } ?>
-
-   <div id="gallery-lb" class="lightbox">
-      <span class="close cursor" id='closelb'>&times;</span>
-      <div class="modal-content">
-
-   <?php
-   if( $query->have_posts() ) {
-       while ( $query->have_posts() ) {
-         $query->the_post();
-
-         $galleribild = get_field('galleribild');
-         $resized = $galleribild['sizes'][ 'grid_thumbnail' ];
-         $bildid = $galleribild['id'];
-
-   ?>
-
-         <div class='galleripost' id='<?php echo $bildid ?>'>
-             <div class='galleribild' style='background-image:url(<?php echo $resized ?>)'></div>
-         </div>
-
-       <?php }
-      } ?>
-
+            <?php }
+           } ?>
         </div>
-    </div>
+
+     <!--LIGHTBOX TERRITORY-->
+
+       <div id="gallery-lb" class="lightbox">
+          <span class="close cursor" id='closelb'>&times;</span>
+          <div class="modal-content">
+
+       <?php
+       if( $query->have_posts() ) {
+           while ( $query->have_posts() ) {
+             $query->the_post();
+
+             $galleribild = get_field('galleribild');
+             $resized = $galleribild['sizes'][ 'grid_thumbnail' ];
+             $bildid = $galleribild['id'];
+
+       ?>
+
+             <div class='galleripost-lb' id='<?php echo $bildid ?>'>
+                 <div class='galleribild-lb' style='background-image:url(<?php echo $resized ?>)'></div>
+             </div>
+
+           <?php }
+          } ?>
+
+                <!-- Next/previous controls -->
+                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+                <!-- Caption text -->
+                <div class="caption-container">
+
+        <?php
+            if( $query->have_posts() ) {
+                while ( $query->have_posts() ) {
+                  $query->the_post();
+                  ?>
+                  
+                  <h2>
+                    <?php the_field('galleripost_rubrik') ?>
+                  </h2>
+                  <p id="caption">
+                      <?php the_field('galleripost_bildtext') ?>
+                  </p>
+
+                  <?php }
+                  } ?>
+
+                </div>
+
+            </div>
+        </div>
 
 </section>
 
--- js/scripts.js
+++ js/scripts.js
@@ -18,9 +18,9 @@ $(document).ready(function() {
           $('#menu-items').removeClass("menu-open");
       }
 //...and while we're at it, the same with a gallery post in the lightbox (see further down)
-      if ($(".galleripost").hasClass('enlarged')) {
+      if ($(".lightbox").hasClass('open')) {
 
-          $(".galleripost").removeClass('enlarged');
+          $(".lightbox").removeClass('open');
       }
 
   });
@@ -35,17 +35,59 @@ $(document).ready(function() {
 
 $(".galleripost").on("click", function(e){
 
-  if ($(".galleripost").hasClass('enlarged')) {
-    $(".galleripost").removeClass('enlarged');
+  var thumbid = $(this).attr('id');
+  var slides = document.getElementsByClassName("galleripost-lb");
+
+  if ($(".galleripost-lb").hasClass('visible')) {
+      $(".galleripost-lb").removeClass('visible');
   }
 
-  $(this).toggleClass('enlarged');
+  $('.lightbox').addClass('open');
+
+  for (var i = 0; i < slides.length; i++) {
+    if (slides[i].id == thumbid ) {
+      console.log(slides[i]);
+      slides[i].classList.add('visible');
+    }
+
+  }
+
 
 });
 
-//Making sure we can still click stuff within the gallery post
+$('#closelb').on("click", function(e){
+
+  $('.lightbox').removeClass('open');
+
+
+});
+
+//Making sure we can still click stuff within the gallery post, and controls within the lightbox
 $(".galleripost").click(function(e) {
   event.stopPropagation();
 });
+$(".modal-content").click(function(e) {
+  event.stopPropagation();
+});
+
+//slide controls:
+
+
+
+// Next/previous controls
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("galleripost-lb");
+//   var captionText = document.getElementById("caption");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//
+//   slides[slideIndex-1].style.display = "block";
+// }
+
 
 });
